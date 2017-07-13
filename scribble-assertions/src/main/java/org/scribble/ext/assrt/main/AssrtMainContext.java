e90dc144e1e855e94ee4713e292988b12647169f
Ray Hu <raymond.hu@imperial.ac.uk>
2017-07-13 14:49:30 +0100

    Added scribble-assertions to root pom exclude (hacky). And removed boiler plate copyright from scrib-assert files.

--- scribble-assertions/src/main/java/org/scribble/ext/assrt/main/AssrtMainContext.java
+++ scribble-assertions/src/main/java/org/scribble/ext/assrt/main/AssrtMainContext.java
@@ -1,16 +1,3 @@
-/**
- * Copyright 2008 The Scribble Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
 package org.scribble.ext.assrt.main;
 
 import java.nio.file.Path;
@@ -34,18 +21,18 @@ public class AssrtMainContext extends MainContext
 {
 	// Load main module from file system
 	public AssrtMainContext(boolean debug, ResourceLocator locator, Path mainpath, boolean useOldWF, boolean noLiveness, boolean minEfsm,
-			boolean fair, boolean noLocalChoiceSubjectCheck, boolean noAcceptCorrelationCheck, boolean noValidation, boolean f17)
+			boolean fair, boolean noLocalChoiceSubjectCheck, boolean noAcceptCorrelationCheck, boolean noValidation)
 					throws ScribParserException, ScribbleException
 	{
-		super(debug, locator, mainpath, useOldWF, noLiveness, minEfsm, fair, noLocalChoiceSubjectCheck, noAcceptCorrelationCheck, noValidation, f17);
+		super(debug, locator, mainpath, useOldWF, noLiveness, minEfsm, fair, noLocalChoiceSubjectCheck, noAcceptCorrelationCheck, noValidation);
 	}
 
 	// For inline module arg
 	public AssrtMainContext(boolean debug, ResourceLocator locator, String inline, boolean useOldWF, boolean noLiveness, boolean minEfsm,
-			boolean fair, boolean noLocalChoiceSubjectCheck, boolean noAcceptCorrelationCheck, boolean noValidation, boolean f17)
+			boolean fair, boolean noLocalChoiceSubjectCheck, boolean noAcceptCorrelationCheck, boolean noValidation)
 					throws ScribParserException, ScribbleException
 	{
-		super(debug, locator, inline, useOldWF, noLiveness, minEfsm, fair, noLocalChoiceSubjectCheck, noAcceptCorrelationCheck, noValidation, f17);
+		super(debug, locator, inline, useOldWF, noLiveness, minEfsm, fair, noLocalChoiceSubjectCheck, noAcceptCorrelationCheck, noValidation);
 		throw new RuntimeException("[scrib-assert] Shouldn't get in here:\n" + inline);
 	}
 
