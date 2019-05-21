b874cacb848f0900093a0d90812b53ab4f2dd49e
Ray Hu <raymond.hu@imperial.ac.uk>
2019-05-21 07:38:46 +0100

    [WIP] Continued refactoring AssrtCoreGTypeTranslator.

---
+++ scribble-assertions/src/main/java/org/scribble/ext/assrt/core/lang/global/AssrtCoreGProtocol.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright 2008 The Scribble Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.scribble.ext.assrt.core.lang.global;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.antlr.runtime.tree.CommonTree;
+import org.scribble.core.job.Core;
+import org.scribble.core.lang.ProtoMod;
+import org.scribble.core.lang.SubprotoSig;
+import org.scribble.core.lang.global.GProtocol;
+import org.scribble.core.lang.local.LProjection;
+import org.scribble.core.type.kind.Global;
+import org.scribble.core.type.kind.Local;
+import org.scribble.core.type.kind.NonRoleParamKind;
+import org.scribble.core.type.name.GProtoName;
+import org.scribble.core.type.name.LProtoName;
+import org.scribble.core.type.name.MemberName;
+import org.scribble.core.type.name.RecVar;
+import org.scribble.core.type.name.Role;
+import org.scribble.core.type.session.global.GRecursion;
+import org.scribble.core.type.session.global.GSeq;
+import org.scribble.core.type.session.local.LSeq;
+import org.scribble.core.visit.STypeInliner;
+import org.scribble.core.visit.STypeUnfolder;
+import org.scribble.core.visit.Substitutor;
+import org.scribble.core.visit.gather.RoleGatherer;
+import org.scribble.core.visit.global.InlinedProjector;
+import org.scribble.util.ScribException;
+
+public class AssrtCoreGProtocol extends GProtocol
+{
+	public AssrtCoreGProtocol(CommonTree source, List<ProtoMod> mods,
+			GProtoName fullname, List<Role> rs,
+			List<MemberName<? extends NonRoleParamKind>> ps, GSeq def)
+	{
+		super(source, mods, fullname, rs, ps, def);
+	}
+
+	@Override
+	public AssrtCoreGProtocol reconstruct(CommonTree source,
+			List<ProtoMod> mods, GProtoName fullname, List<Role> rs,
+			List<MemberName<? extends NonRoleParamKind>> ps, GSeq def)
+	{
+		return new AssrtCoreGProtocol(source, mods, fullname, rs, ps, def);
+	}
+	
+	// Cf. (e.g.) checkRoleEnabling, that takes Core
+	// CHECKME: drop from Protocol (after removing Protocol from SType?)
+	// Pre: stack.peek is the sig for the calling Do (or top-level entry), i.e., it gives the roles/args at the call-site
+	@Override
+	public AssrtCoreGProtocol getInlined(STypeInliner<Global, GSeq> v)
+	{
+		SubprotoSig sig = new SubprotoSig(this);
+		v.pushSig(sig);
+
+		Substitutor<Global, GSeq> subs = v.core.config.vf.Substitutor(this.roles, sig.roles,
+				this.params, sig.args);
+		GSeq inlined = v.visitSeq(subs.visitSeq(this.def));
+		RecVar rv = v.getInlinedRecVar(sig);
+		GRecursion rec = v.core.config.tf.global.GRecursion(null, rv, inlined);  // CHECKME: or protodecl source?
+		GSeq seq = v.core.config.tf.global.GSeq(null, Arrays.asList(rec));
+		GSeq def = v.core.config.vf.<Global, GSeq>RecPruner().visitSeq(seq);
+		Set<Role> used = def.gather(new RoleGatherer<Global, GSeq>()::visit)
+				.collect(Collectors.toSet());
+		List<Role> rs = this.roles.stream().filter(x -> used.contains(x))  // Prune role decls -- CHECKME: what is an example? was this from before unused role checking?
+				.collect(Collectors.toList());
+		return //new GProtocol
+				reconstruct(getSource(), this.mods, this.fullname, rs,
+				this.params, def);
+	}
+	
+	@Override
+	public AssrtCoreGProtocol unfoldAllOnce(STypeUnfolder<Global, GSeq> v)
+	{
+		throw new RuntimeException("Deprecated for " + getClass() + ":\n" + this);
+	}
+
+	@Override
+	public void checkRoleEnabling(Core core) throws ScribException
+	{
+		throw new RuntimeException("Deprecated for " + getClass() + ":\n" + this);
+	}
+
+	@Override
+	public void checkExtChoiceConsistency(Core core) throws ScribException
+	{
+		throw new RuntimeException("Deprecated for " + getClass() + ":\n" + this);
+	}
+
+	public void checkConnectedness(Core core, boolean implicit)
+			throws ScribException
+	{
+		throw new RuntimeException("Deprecated for " + getClass() + ":\n" + this);
+	}
+	
+	// Currently assuming inlining (or at least "disjoint" protodecl projection, without role fixing)
+	public LProjection projectInlined(Core core, Role self)
+	{
+		LSeq def = core.config.vf.global.InlinedProjector(core, self)
+				.visitSeq(this.def);
+		LSeq fixed = core.config.vf.local.InlinedExtChoiceSubjFixer().visitSeq(def);
+		return projectAux(core, self, this.roles, fixed);
+	}
+	
+	// Does rec and role pruning
+	private LProjection projectAux(Core core, Role self, List<Role> decls,
+			LSeq def)
+	{
+		LSeq pruned = core.config.vf.<Local, LSeq>RecPruner().visitSeq(def);
+		LProtoName fullname = InlinedProjector
+				.getFullProjectionName(this.fullname, self);
+		Set<Role> used = pruned.gather(new RoleGatherer<Local, LSeq>()::visit)
+				.collect(Collectors.toSet());
+		List<Role> roles = decls.stream()
+				.filter(x -> x.equals(self) || used.contains(x))
+				.collect(Collectors.toList());
+		List<MemberName<? extends NonRoleParamKind>> params =
+				new LinkedList<>(this.params);  // CHECKME: filter params by usage?
+		return new LProjection(this.mods, fullname, roles, self, params,
+				this.fullname, pruned);  // CHECKME: add/do via tf?
+	}
+
+	// N.B. no "fixing" passes done here -- need breadth-first passes to be sequentialised for subproto visiting
+	public LProjection project(Core core, Role self)
+	{
+		throw new RuntimeException("Deprecated for " + getClass() + ":\n" + this);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return this.mods.stream().map(x -> x.toString() + " ")
+				.collect(Collectors.joining()) + "global " + super.toString();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int hash = 25799;
+		hash = 31 * hash + super.hashCode();
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (!(o instanceof AssrtCoreGProtocol))
+		{
+			return false;
+		}
+		return super.equals(o);  // Does canEquals
+	}
+
+	@Override
+	public boolean canEquals(Object o)
+	{
+		return o instanceof AssrtCoreGProtocol;
+	}
+}
