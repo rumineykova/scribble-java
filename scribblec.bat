12acb11f6df01dd37718ac45b98a0efdf8f4248e
Rumyana Neykova (Staff) <csstrrn@brunel.ac.uk>
2019-08-29 20:17:57 +0100

    Add missing bat files (add missing classpath)

--- scribblec.bat
+++ scribblec.bat
@@ -6,7 +6,7 @@ set DIR=%~dp0%
 
 set LIB=scribble-dist\target\lib
 
-set CLASSPATH=%DIR%\scribble-cli\target\classes\;%DIR%\scribble-core\target\classes;%DIR%\scribble-parser\target\classes;%DIR%\scribble-codegen\target\classes;%ANTLR%;%DIR%\%LIB%\antlr.jar;%DIR%\%LIB%\antlr-runtime.jar;%DIR%\%LIB%\commons-io.jar;%DIR%\%LIB%\scribble-cli.jar;%DIR%\%LIB%\scribble-core.jar;%DIR%\%LIB%\scribble-parser.jar;%DIR%\%LIB%\scribble-codegen.jar;%DIR%\%LIB%\stringtemplate.jar
+set CLASSPATH=%DIR%\scribble-cli\target\classes\;%DIR%\scribble-main\target\classes;%DIR%\scribble-ast\target\classes;%DIR%\scribble-core\target\classes;%DIR%\scribble-parser\target\classes;%DIR%\scribble-codegen\target\classes;%ANTLR%;%DIR%\%LIB%\antlr.jar;%DIR%\%LIB%\antlr-runtime.jar;%DIR%\%LIB%\commons-io.jar;%DIR%\%LIB%\scribble-cli.jar;%DIR%\%LIB%\scribble-core.jar;%DIR%\%LIB%\scribble-parser.jar;%DIR%\%LIB%\scribble-codegen.jar;%DIR%\%LIB%\stringtemplate.jar
 
 
 java -cp "%CLASSPATH%" org.scribble.cli.CommandLine %*
--- scribblecass.bat
+++ scribblecass.bat
@@ -12,6 +12,6 @@ set JAVASMT=%USERPROFILE%\.m2\repository\org\sosy-lab\java-smt\1.0.1\java-smt-1.
 
 set LIB=scribble-dist\target\lib
 
-set CLASSPATH=%Z3%;%SMTZ3%;%JAVASMT%;%DIR%\scribble-assertions\target\classes;\%DIR%\scribble-cli\target\classes\;%DIR%\scribble-core\target\classes;%DIR%\scribble-parser\target\classes;%DIR%\scribble-codegen\target\classes;%ANTLR%;%DIR%\%LIB%\antlr.jar;%DIR%\%LIB%\antlr-runtime.jar;%DIR%\%LIB%\commons-io.jar;%DIR%\%LIB%\scribble-cli.jar;%DIR%\%LIB%\scribble-core.jar;%DIR%\%LIB%\scribble-parser.jar;%DIR%\%LIB%\scribble-codegen.jar;%DIR%\%LIB%\stringtemplate.jar
+set CLASSPATH=%Z3%;%SMTZ3%;%JAVASMT%;%DIR%\scribble-assertions\target\classes;\%DIR%\scribble-main\target\classes;\%DIR%\scribble-ast\target\classes\;\%DIR%\scribble-cli\target\classes\;%DIR%\scribble-core\target\classes;%DIR%\scribble-parser\target\classes;%DIR%\scribble-codegen\target\classes;%ANTLR%;%DIR%\%LIB%\antlr.jar;%DIR%\%LIB%\antlr-runtime.jar;%DIR%\%LIB%\commons-io.jar;%DIR%\%LIB%\scribble-cli.jar;%DIR%\%LIB%\scribble-core.jar;%DIR%\%LIB%\scribble-parser.jar;%DIR%\%LIB%\scribble-codegen.jar;%DIR%\%LIB%\stringtemplate.jar
 
 java -cp "%CLASSPATH%" org.scribble.ext.assrt.cli.AssrtCommandLine %*
