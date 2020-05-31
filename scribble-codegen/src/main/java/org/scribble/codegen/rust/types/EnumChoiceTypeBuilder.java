b211d056a3626565c33f351261116d1bd3856038
rumi <rumi.neykova@gmail.com>
2020-06-01 00:28:10 +0100

    Add two new Type Builders for choice and offer

---
+++ scribble-codegen/src/main/java/org/scribble/codegen/rust/types/EnumChoiceTypeBuilder.java
@@ -0,0 +1,117 @@
+package org.scribble.codegen.rust.types;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.scribble.type.name.Role;
+
+public class EnumChoiceTypeBuilder implements IRustMpstBuilder {
+	private String finalTypeName; 
+	private Map<Role, String> finalTypeNames = new HashMap<>(); 
+	public ArrayList<IRustMpstBuilder> paths = new ArrayList<>();
+	BuilderKind kind; 
+	String execOrderName; 
+	int counter; 
+	public Map<Role, String> rolesToNames = new HashMap<>(); 
+	Role self; 
+	List<Role> otherRoles = new ArrayList<Role>();
+	
+	public EnumChoiceTypeBuilder(ArrayList<IRustMpstBuilder> paths, BuilderKind kind, Role self, List<Role> otherRoles) {
+		this.paths = paths;
+		this.kind = kind; 
+		this.self = self; 
+		this.otherRoles = otherRoles; 
+		for (int i=0; i< otherRoles.size(); i++) {
+		this.finalTypeNames.put(otherRoles.get(i),
+				String.format("Choose%sfor%sto%s<N>", this.self, 
+						otherRoles.get(i), this.self)); 
+		}
+	}
+
+	public BuilderKind getKind() {
+		return this.kind;
+	}
+	// example: 
+	//type ChooseCtoB<N> = ChooseMpst<AtoBVideo<N>, CtoBClose, AtoBClose, CtoBClose, QueueBVideoDual, QueueBEnd>;
+	// as many ChooseTypes as the OtherRoles
+	private String buildMpstChoice() {
+		StringBuilder sb = new StringBuilder(); 
+		StringBuilder qb = new StringBuilder(); 
+		String simpleTypes =  this.paths.stream()
+				.map(t -> t.build() + "\n").reduce("", String::concat);
+		sb.append(simpleTypes);
+		this.execOrderName = String.format("Ordering%sChoice", this.self.toString());
+	    //type QueueCChoice = RoleCtoAll<QueueCVideo, QueueCEnd>;
+	    String choiceQueue = 
+	    		String.format("type %s = Role%stoAll<%s, %s>;",
+	    		this.execOrderName, this.self.toString(), 
+	    		((RustMpstSessionBuilder)this.paths.get(0)).execOrderName, 
+	    		((RustMpstSessionBuilder)this.paths.get(1)).execOrderName);
+	    qb.append(choiceQueue);
+	    
+		//collect(Collectors.toList());
+	    /*
+		for (int i=0;i<this.otherRoles.size(); i++) {
+			Role role = this.otherRoles.get(i);
+			String name = String.format("%s%sto%s<N>", this.getKindString(), 
+					this.self.toString(), role);
+			this.finalTypeNames.put(role, name); 
+			String decl = String.format("type %s = ChooseMpst<", name);
+			StringBuilder declBuilder = new StringBuilder(); 
+			declBuilder.append(decl);
+	
+			StringBuilder sbQ = new StringBuilder(); 
+			List<String> binaryNames =  this.paths.stream()
+						.map(t -> ((RustMpstSessionBuilder)t).rolesToTypeNames.get(role))
+						.collect(Collectors.toList());
+			for (int j=0;j<binaryNames.size(); j++) {
+				declBuilder.append("End," + binaryNames.get(j) + ",");
+				sbQ.append(((RustMpstSessionBuilder)this.paths.get(j)).execOrderName + ",");
+			}
+			sb.append("\n").append(declBuilder);
+			sb.append(sbQ);
+			sb.deleteCharAt(sb.length()-1);
+			sb.append(">;");}
+			//return sb.toString();
+			return qb.toString();*/
+	    return qb.toString();
+	}
+	
+	public int getCounter() {
+		return Util.getNextCounter(this.self);
+	}
+	
+	public String getKindString() {
+		if (this.kind== BuilderKind.Choice) return "Choice";
+		return  "Offer";
+	}
+	
+	@Override
+	public String build() {
+		return "";//this.buildMpstChoice();
+	}
+	
+	public int getNext() {
+		return this.counter++; 
+	}
+	@Override
+	public String getFinalTypeName() {
+		return this.finalTypeName;
+	}
+	
+	public String getFinalTypeNameByRole(Role r) {
+		return this.finalTypeNames.get(r);
+	}
+	
+	public String getExecuteOrderName() {
+		return "RoleEnd";//this.execOrderName;
+	}
+
+	@Override
+	public Role getSelf() {
+		return this.self;
+	}
+}
---
+++ scribble-codegen/src/main/java/org/scribble/codegen/rust/types/EnumOfferTypeBuilder.java
@@ -0,0 +1,131 @@
+package org.scribble.codegen.rust.types;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.scribble.type.name.Role;
+
+public class EnumOfferTypeBuilder implements IRustMpstBuilder {
+	private String finalTypeName; 
+	public ArrayList<IRustMpstBuilder> paths = new ArrayList<>();
+	public ArrayList<String> lables = new ArrayList<>();
+	BuilderKind kind; 
+	int counter; 
+	public Map<Role, String> rolesToNames = new HashMap<>(); 
+	Role self; 
+	Role controlRole; 
+	
+	public EnumOfferTypeBuilder(ArrayList<IRustMpstBuilder> paths, 
+			ArrayList<String> labels, 
+			BuilderKind kind, Role self, Role controlRole) {
+		this.paths = paths;
+		this.kind = kind; 
+		this.self = self; 
+		this.lables = labels;
+		this.controlRole = controlRole; 
+	}
+	@Override
+	public BuilderKind getKind() {
+		return this.kind;
+	}
+	/*
+	enum CBranchesAtoC<N: marker::Send> {
+	    End(SessionMpst<AtoBClose, AtoCClose, QueueAEnd>),
+	    Video(SessionMpst<AtoBVideo<N>, Recv<N, Send<N, RecursAtoC<N>>>, QueueAVideo>),
+	}
+	enum CBranchesBtoC<N: marker::Send> {
+	    End(SessionMpst<BtoAClose, BtoCClose, QueueBEnd>),
+	    Video(SessionMpst<BtoAVideo<N>, RecursBtoC<N>, QueueBVideo>),
+	}
+	type ChooseCforAtoC<N> = Send<CBranchesAtoC<N>, End>;
+	type ChooseCforBtoC<N> = Send<CBranchesBtoC<N>, End>;
+	*/
+	private String buildMpstOffer() {
+		StringBuilder sb = new StringBuilder(); 
+		String name = String.format("CBranches%stoC", this.self);
+		this.finalTypeName = name + "<N>"; 
+		String decl = String.format("enum %s<N: marker::Send> { \n", name);
+		StringBuilder declBuilder = new StringBuilder(); 
+		declBuilder.append(decl);
+
+		StringBuilder sbQ = new StringBuilder(); 
+		for (int i=0;i<this.paths.size();i++) { 
+			declBuilder.append(this.lables.get(i) + "(SessionMpst<");
+			RustMpstSessionBuilder simpleType = 
+					(RustMpstSessionBuilder) this.paths.get(i);	
+			String simpleTypeString = simpleType.build();
+			String binaryTypes = simpleType.rolesToTypeNames
+					.values()
+					.stream()
+					.map(t -> t + ",")
+					.reduce("", String::concat);
+			 sb.append(simpleTypeString + "\n");
+			declBuilder.append(binaryTypes);
+			declBuilder.append(simpleType.execOrderName + ">), \n");
+		}
+		sb.append("\n").append(declBuilder.toString());
+		sb.append("}").append("\n");
+		sb.append(String.format("type %s = Send<%s, End>; \n", this.finalTypeName, 
+				this.finalTypeName));
+		return sb.toString();
+	}
+	
+	private String buildMpstChoice() {
+		StringBuilder sb = new StringBuilder(); 
+		String name = String.format("Choice%sto%s<N>", 
+				 this.controlRole, this.self.toString());
+		String decl = String.format("type %s = ChooseMpst<", name, this.getKindString());
+		StringBuilder declBuilder = new StringBuilder(); 
+		declBuilder.append(decl);
+
+		StringBuilder sbQ = new StringBuilder(); 
+		for (int i=0;i<this.paths.size();i++) { 
+			RustMpstSessionBuilder simpleType = 
+					(RustMpstSessionBuilder) this.paths.get(i);	
+			String simpleTypeString = simpleType.build();
+			String binaryTypes = simpleType.rolesToTypeNames
+					.values()
+					.stream()
+					.map(t -> t!="End"? "<" + t + " as Session>::Dual,":t + ",")
+					.reduce("", String::concat);
+			//sb.append(simpleTypeString + "\n");
+			declBuilder.append(binaryTypes);
+			sbQ.append(simpleType.execOrderName + "::Dual,");
+		}
+		
+		sb.append("\n").append(declBuilder.toString());
+		sb.append(sbQ);
+		sb.deleteCharAt(sb.length()-1);
+		sb.append(">;");
+		return sb.toString();
+	}
+	
+	public int getCounter() {
+		return Util.getNextCounter(this.self);
+	}
+	
+	public String getKindString() {
+		if (this.kind== BuilderKind.Choice) return "Choice";
+		return  "Offer";
+	}
+	
+	@Override
+	public String build() {
+		return this.buildMpstOffer();
+	}
+	
+	public int getNext() {
+		return this.counter++; 
+	}
+	@Override
+	public String getFinalTypeName() {
+		return this.finalTypeName;
+	}
+	@Override
+	public Role getSelf() {
+		return this.self;
+	}
+
+}
